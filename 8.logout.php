59a95fb02f15c876887cdeecb226d30f03342042
qwopcgj <64061999+qwopcgj@users.noreply.github.com>
2021-01-06 20:56:21 +0800

    Update 8.logout.php

--- 8.logout.php
+++ 8.logout.php
@@ -1,9 +1,9 @@
-<？php
+<?php
 
-session_start（）;
-//將會話清空
-unset（$ _SESSION [ “ id” ]）;
-迴聲 “登出中......” ;
-迴聲 '<meta http-equiv = REFRESH CONTENT = 2; url = login.html>' ;
+session_start();
+//將session清空
+unset($_SESSION["id"]);
+echo '登出中......';
+echo '<meta http-equiv=REFRESH CONTENT=2;url=login.html>';
 
-？>
+?>
