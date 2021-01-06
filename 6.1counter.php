da508e8652bacdecdff4ba86888fb5121d0a6aad
qwopcgj <64061999+qwopcgj@users.noreply.github.com>
2021-01-06 21:00:41 +0800

    Update 6.1counter.php

--- 6.1counter.php
+++ 6.1counter.php
@@ -1,10 +1,10 @@
-<？php
-    error_reporting（0）;
-    session_start（）;
-    if（！isset（$ _SESSION [ “ counter1” ]））{
-        $ _SESSION [ “ counter1” ] = 1 ;
-    }其他{
-        $ _SESSION [ “ counter1” ] ++;
+<?php
+    error_reporting(0);
+    session_start();
+    if (!isset($_SESSION["counter1"])){
+        $_SESSION["counter1"]=1;
+    }else{
+        $_SESSION["counter1"]++;
     }
-    echo  “登錄{$ _SESSION [” counter1 “]}人次” ;
-？>
+    echo "登入{$_SESSION["counter1"]}人次";
+?>
